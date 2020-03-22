b16855357feef0ed500438da17e8adb72dfd92cb
Ido Mamia <48285277+idomamia@users.noreply.github.com>
2020-03-22 22:51:10 +0200

    feat(angular-material): Add Angular Material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+
 
 import { AppComponent } from './app.component';
+import { MergeRequestCardComponent } from './merge-request-card/merge-request-card.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MergeRequestCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
