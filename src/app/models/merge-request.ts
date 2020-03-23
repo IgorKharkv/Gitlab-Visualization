df4d7b06d7f9d2b7cc44e42a307b5340169d8071
Ido Mamia <48285277+idomamia@users.noreply.github.com>
2020-03-23 11:07:17 +0200

    chore(models): Add models to the project
    Add models that fit the data received from the Gitlab API.

---
+++ src/app/models/merge-request.ts
@@ -0,0 +1,19 @@
+import {User} from './user';
+
+export interface MergeRequest {
+  id: number;
+  iid: number;
+  project_id: number;
+  title: string;
+  description: string;
+  state: string;
+  created_at: string;
+  updated_at: string;
+  target_branch: string;
+  source_branch: string;
+  upvotes: number;
+  downvotes: number;
+  author: User;
+  work_in_progress: boolean;
+  web_url: string;
+}
