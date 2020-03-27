3b185debe0c39cb35d32c651af27596b43711472
Ido Mamia <48285277+idomamia@users.noreply.github.com>
2020-03-27 12:17:35 +0300

    feat(filter-by teams): Add a filter by teams and a loading gif
    The filtering is in the team-tab component.
    In order to make only one http request, the request is being done in the app component.
    In addition, a loading gif has been added.

--- src/app/team-tab/team-tab.component.ts
+++ src/app/team-tab/team-tab.component.ts
@@ -1,18 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {GitApiService} from '../services/git-api.service';
+import teamsJson from '../../assets/teams.json';
+import {MergeRequest} from '../models/merge-request';
 
 @Component({
   selector: 'app-team-tab',
   templateUrl: './team-tab.component.html',
   styleUrls: ['./team-tab.component.css']
 })
-export class TeamTabComponent implements OnInit {
+export class TeamTabComponent implements OnChanges {
 
   @Input() team: string;
+  @Input() mergeRequests: MergeRequest[];
 
-  constructor(private api: GitApiService) { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.mergeRequests) {
+      this.mergeRequests = this.mergeRequests.filter(mr => this.getTeamMembers().find(member => member === mr.author.username));
+    }
+  }
+
+  getTeamMembers(): string[] {
+    return teamsJson.find((team) => team.name === this.team).members;
+  }
 }
